ef975aeca98ae3ce3f33f081196bb4ac8b55f45c
gdiao <gary.diao@yahoo.com>
2017-07-12 15:47:02 +0800

    update the topic page layout

---
+++ source/js/views/wgroup/WGroupTopicPage.jsx
@@ -0,0 +1,369 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import ActGavel from 'material-ui/svg-icons/action/gavel';
+import ActDone from 'material-ui/svg-icons/action/done';
+import NviArrowDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import NviArrowUp from 'material-ui/svg-icons/navigation/arrow-drop-up';
+import ActThumbsUpDown from 'material-ui/svg-icons/action/thumbs-up-down';
+import ActBookmark from 'material-ui/svg-icons/action/bookmark';
+import ActTrackChng from 'material-ui/svg-icons/action/track-changes';
+import ActVisibility from 'material-ui/svg-icons/action/visibility';
+import ContentFlag from 'material-ui/svg-icons/content/flag';
+import SocialShare from 'material-ui/svg-icons/social/share';
+import TglStar from 'material-ui/svg-icons/toggle/star';
+import TglStarBorder from 'material-ui/svg-icons/toggle/star-border';
+import ImageLens from 'material-ui/svg-icons/image/lens';
+import AVStop from 'material-ui/svg-icons/av/stop';
+import EditBubbleChrt from 'material-ui/svg-icons/editor/bubble-chart';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import NavFirstPage from 'material-ui/svg-icons/navigation/first-page';
+import NavLastPage from 'material-ui/svg-icons/navigation/last-page';
+import IconButton from 'material-ui/IconButton';
+
+import { snackAction, loaderAction } from '../../store/actions/appActions';
+import PageHeaderBar from '../component/PageHeaderBar';
+
+function getStyles(muiTheme) {
+  const { baseTheme:{ palette } } = muiTheme;
+
+  return {
+    pageHeader: {
+      backgroundColor: palette.primary1Color,
+    },
+    switchButton:{
+      color: palette.primary2Color,
+    },
+    title:{
+      textDecoration: 'none',
+      color: palette.textColor,
+    },
+    leftPanel: {
+      flex: 1,
+      paddingRight: 10,
+    },
+    rightPanel: {
+      paddingLeft: 10,
+      flexGrow: 0,
+      flexShrink: 0,
+      flexBasis: 300,
+    },
+    iconBtn: {
+      color: palette.primary2Color,
+    },
+    thumbInfo: {
+      color: palette.secondaryTextColor,
+      textAlign: 'center',
+      fontWeight: 600,
+    },
+    answerSortBtn:{padding:12, width:44, height:44},
+    answerSortBtnIcon:{ width:20, height:20 ,color: palette.primary2Color},
+    timeStamp: {
+      color: palette.accent3Color,
+      marginRight: 10,
+      float: 'right',
+      fontSize: 12
+    },
+    post: {
+      borderBottomColor: palette.borderColor,
+      borderBottomWidth: 1,
+      borderBottomStyle: 'solid',
+      paddingTop: '1rem'
+    },
+    answer:{
+      paddingTop: 10,
+      borderBottomColor: palette.borderColor,
+      borderBottomWidth: 1,
+      borderBottomStyle: 'solid',
+    },
+    sumTitle:{
+      color: palette.accent3Color,
+      display: 'block',
+      fontSize: 12
+    },
+    comments: {
+      position: 'relative'
+    }, 
+    comment: {
+      display: 'flex', 
+      padding: 10,
+      borderTopColor: palette.borderColor,
+      borderTopWidth: 1,
+      borderTopStyle: 'solid',
+    },
+    account: {
+      textDecoration: 'none' ,
+      marginRight: 5
+    }
+  };
+}
+
+class WGroupTopicPage extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {collapsed: false};
+    this.styles = getStyles(props.muiTheme);
+  }
+
+  onCollapseSwitch = () =>{
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+
+  onTest = () => {
+    this.props.resetRootMenu({menuPaneVisible:true, menuPane: (<RootMenuContent test1={this.onTest1}/>) });
+  }
+
+  onTest1 = () => {
+    console.log('12------3')
+  }
+
+
+  componentDidMount(){
+    //this.props.resetRootMenu({menuPaneVisible:true, menuPane: null });
+    this.props.resetRootMenu({menuPaneVisible:true, menuPane: (<RootMenuContent test1={this.onTest1}/>) });
+    //this.props.resetRootMenu({menuPaneVisible:false, menuPane: (<RootMenuContent test1={this.onTest1}/>) });
+  }
+
+  render() {
+    const { muiTheme } = this.props;
+    const styles = this.styles;
+    return (
+      <div className="page-wrapper">
+        <header className="page-header-wrapper" style={this.styles.pageHeader}>
+          <PageHeaderBar muiTheme={ muiTheme }/>
+        </header>
+        <div className="page-content-wrapper">
+          <div className="page-right-menu">
+            <div className={ this.state.collapsed ? "right-menu collapsed":"right-menu " }>
+              <div className="menu-body">
+              </div>
+              <div className="menu-footer">
+                <IconButton onTouchTap={ this.onCollapseSwitch } iconStyle={this.styles.switchButton}>
+                  {this.state.collapsed ? <NavFirstPage /> : <NavLastPage/>}
+                </IconButton>
+              </div>
+            </div>
+          </div>
+          <div className="page-content" style={{overflowY:'auto', padding:'1rem'}}>
+          <div style={ styles.post }>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flexBasis: 60, display: 'flex', flexDirection:'column', paddingLeft: 10, paddingRight:10}}>
+                <Avatar src="assets/img/kerem-128.jpg" size={40} style={{ marginLeft:5}} />
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowUp/></IconButton>
+                <div style={ styles.thumbInfo }>
+                  1023
+                </div>
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowDown/></IconButton>
+                <IconButton iconStyle={ styles.iconBtn }><TglStarBorder/></IconButton>
+              </div>
+              <div style={{ flex: 1 }}>
+                <div>
+                  <a href="/sf" style={ styles.account }>
+                    <span style={{ fontWeight: 600}}>akuzko</span> Artme Kuzko
+                  </a> 
+                  <span style={ { display: 'inline-block', height: 18} }>
+                    <AVStop style={ { width: 16, height: 16, color: 'red', float: 'left', marginTop: 4, marginRight: 3 } } />
+                     Develop
+                  </span>
+                  <span style={ styles.timeStamp }>6days</span>
+                </div>
+                <div className="post-content">
+                  <p>Hey guys!</p>
+                  <p>If you use vanilla redux in your app and ever got tired writing structures like</p>
+                  <p>I understand there are things like React's Immutability Helpers and <code>immutable-js</code>, but if you don't want to have additional data abstraction layer and just want simple helpers with, basically, no special API, <code>update-js</code> might serve you well.</p>
+                  <p>Any comments and feedback are very welcome.</p>
+                  <p>Thanks and happy coding!</p>
+                </div>
+                <div style={ styles.comments }>
+                  <div style={{ position:'absolute', right: 10, top:-45, width:40}}>
+                    <IconButton style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                      <ContentAdd/>
+                    </IconButton>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1,verticalAligh: 'top', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches of the 
+                     non-whitespace chunks that have some horizontal whitespaces on the 
+                     subsequent lines after <code>Main :</code>?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1, verticalAligh: 'top', textAlign: 'left', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches ?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style={{ padding:'0 0.5rem', position:'relative' }}>
+            <h4 style={{ padding: '1rem 0 0.5rem', lineHeight: '2.5rem' }} >Answer (8) </h4>
+            <div style={{ position:'absolute', right:0, top:'0' }}>
+              <IconButton tooltip="Sort By Activity" style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                <EditBubbleChrt/>
+              </IconButton>
+              <IconButton tooltip="Sort By votes" disabled={true} style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                <ActThumbsUpDown/>
+              </IconButton>
+            </div>
+          </div>
+          <div style={ styles.answer }>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flexBasis: 60, display: 'flex', flexDirection:'column', paddingLeft: 10, paddingRight:10}}>
+                <Avatar src="assets/img/kerem-128.jpg" size={40} style={{ marginLeft:5}}  />
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowUp/></IconButton>
+                <div style={ styles.thumbInfo }>
+                  23
+                </div>
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowDown/></IconButton>
+                <IconButton iconStyle={ styles.iconBtn }><ActDone/></IconButton>
+              </div>
+              <div style={{ flex: 1 }}>
+                <div>
+                  <a href="/sf" style={ styles.account }>
+                    <span style={{ fontWeight: 600}}>akuzko</span> Artme Kuzko
+                  </a> 
+                  <span style={ styles.timeStamp }>6days</span>
+                </div>
+                <div className="answer-content">
+                  <p>Hey guys!</p>
+
+                  <p>If you use vanilla redux in your app and ever got tired writing structures like</p>
+
+                  <p>I understand there are things like React's Immutability Helpers and <code>immutable-js</code>, but if you don't want to have additional data abstraction layer and just want simple helpers with, basically, no special API, <code>update-js</code> might serve you well.</p>
+
+                  <p>Any comments and feedback are very welcome.</p>
+
+                  <p>Thanks and happy coding!</p>
+                </div>
+                
+                <div style={ styles.comments }>
+                  <div style={{ position:'absolute', right: 10, top:-45, width:90}}>
+                    <IconButton style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                      <ActGavel/>
+                    </IconButton>
+                    <IconButton style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                      <ContentAdd/>
+                    </IconButton>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1,verticalAligh: 'top', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches of the 
+                     non-whitespace chunks that have some horizontal whitespaces on the 
+                     subsequent lines after <code>Main :</code>?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1, verticalAligh: 'top', textAlign: 'left', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches ?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div style={ styles.answer }>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flexBasis: 60, display: 'flex', flexDirection:'column', paddingLeft: 10, paddingRight:10}}>
+                <Avatar src="assets/img/kerem-128.jpg" size={40} style={{ marginLeft:5}}  />
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowUp/></IconButton>
+                <div style={ styles.thumbInfo }>
+                  13
+                </div>
+                <IconButton iconStyle={ styles.iconBtn }><NviArrowDown/></IconButton>
+              </div>
+              <div style={{ flex: 1 }}>
+                <div>
+                  <a href="/sf" style={ styles.account }>
+                    <span style={{ fontWeight: 600}}>akuzko</span> Artme Kuzko
+                  </a> 
+                  <span style={ styles.timeStamp }>6days</span>
+                </div>
+                <div className="post-content">
+                  <p>Hey guys!</p>
+
+                  <p>If you use vanilla redux in your app and ever got tired writing structures like</p>
+
+                  <p>I understand there are things like React's Immutability Helpers and <code>immutable-js</code>, but if you don't want to have additional data abstraction layer and just want simple helpers with, basically, no special API, <code>update-js</code> might serve you well.</p>
+
+                  <p>Any comments and feedback are very welcome.</p>
+
+                  <p>Thanks and happy coding!</p>
+                </div>
+                
+                <div style={ styles.comments }>
+                  <div style={{ position:'absolute', right: 10, top:-45, width:40}}>
+                    <IconButton style={ styles.answerSortBtn } iconStyle={ styles.answerSortBtnIcon }>
+                      <ContentAdd/>
+                    </IconButton>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1,verticalAligh: 'top', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches of the 
+                     non-whitespace chunks that have some horizontal whitespaces on the 
+                     subsequent lines after <code>Main :</code>?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                  <div style={ styles.comment }>
+                    <div style={{ flexBasis: 50, height: 30}}>
+                      <Avatar src="assets/img/kerem-128.jpg" size={30} style={{ marginLeft: 5 }} />
+                    </div>
+                    <div style={{ flex:1, verticalAligh: 'top', textAlign: 'left', fontSize: 14, lineHeight: 1.4 }}>
+                     <span >Does it mean you need to get multiple matches ?</span>
+                     <a href="sf" style={ styles.account }>auset</a><span style={ styles.timeStamp }>Dec 20 16 at 23:58</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const RootMenuContent = ({ test1 }) => {
+  
+  return (<div>
+    <RaisedButton label="Test" primary={true} onTouchTap={test1}/>
+  </div>);
+};
+
+export default connect(
+  (state) => ({}),
+  (dispatch) => (
+    bindActionCreators({
+      snackAction,
+      loaderAction,
+    }, dispatch)
+  )
+)(WGroupTopicPage);
